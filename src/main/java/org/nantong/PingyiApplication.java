f7bb4d3e5412e566c601bf7086084b96167544f8
torch <1593028064@qq.com>
2017-05-02 19:09:14 +0800

    set env to dev

---
+++ src/main/java/org/nantong/PingyiApplication.java
@@ -0,0 +1,12 @@
+package org.nantong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingyiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingyiApplication.class, args);
+	}
+}
